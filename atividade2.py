f6761516afc3a8151c399d50d0b95848a4134a80
VFerrari <vferrari@mpc.com.br>
2020-10-06 23:25:03 -0300

    Simplifying subtour elimination

--- atividade2.py
+++ atividade2.py
@@ -5,31 +5,24 @@ from itertools import combinations
 import gurobipy as gp
 from gurobipy import GRB
 
-
 # Callback - use lazy constraints to eliminate sub-tours
 def subtourelim(model, where):
     if where == GRB.Callback.MIPSOL:
-        # make a list of edges selected in the solution
-        vals_x = model.cbGetSolution(model._vars[0])
-        vals_y = model.cbGetSolution(model._vars[1])
-        selected_x = gp.tuplelist((i, j) for i, j in model._vars[0].keys()
-                                if vals_x[i, j] > 0.5)
-        selected_y = gp.tuplelist((i, j) for i, j in model._vars[1].keys()
-                                if vals_y[i, j] > 0.5)
-        # find the shortest cycle in the selected edge list
-        tour_x = subtour(selected_x)
-        tour_y = subtour(selected_y)
-        if len(tour_x) < n:
-            # add subtour elimination constr. for every pair of cities in tour
-            model.cbLazy(gp.quicksum(model._vars[0][i, j]
-                                     for i, j in combinations(tour_x, 2))
-                         <= len(tour_x)-1)
-        if len(tour_y) < n:
-            # add subtour elimination constr. for every pair of cities in tour
-            model.cbLazy(gp.quicksum(model._vars[1][i, j]
-                                     for i, j in combinations(tour_y, 2))
-                         <= len(tour_y)-1)
+        
+        # Do it for both tours.
+        for t in range(2):
+            # make a list of edges selected in the solution
+            vals = model.cbGetSolution(model._vars[t])
+            selected = gp.tuplelist((i, j) for i, j in model._vars[t].keys()
+                                if vals[i, j] > 0.5)
 
+            # find the shortest cycle in the selected edge list
+            tour = subtour(selected)
+            if len(tour) < n:
+                # add subtour elimination constr. for every pair of cities in tour
+                model.cbLazy(gp.quicksum(model._vars[t][i, j]
+                                        for i, j in combinations(tour, 2))
+                            <= len(tour)-1)
 
 # Given a tuplelist of edges, find the shortest subtour
 def subtour(edges):
@@ -49,7 +42,7 @@ def subtour(edges):
     return cycle
 
 
-n = 100
+n = 20
 # Create n random points
 # os valores dos pontos randomicos sao utilizados para definir as distancias euclidianas entre eles
 # definindo assim as arestas, os "nomes" dos vertices sao de 0 a 19.
