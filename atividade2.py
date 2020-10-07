72d9bad7670f9a1f5cecf92a80b12b6ba2af751f
VFerrari <vferrari@mpc.com.br>
2020-10-06 23:48:43 -0300

    Refactoring code and adding gitignore

--- atividade2.py
+++ atividade2.py
@@ -17,12 +17,79 @@ University of Campinas - UNICAMP - 2020
 
 Last Modified: 06/10/2020
 '''
+
 import sys
 import math
 import random
 from itertools import combinations
 import gurobipy as gp
 from gurobipy import GRB
+
+# Create n random points
+# define the position of each point in the plane, in float coordinates [0,1]
+# the vertices' "names" are [0,n-1].
+def generate_instance(n):
+	random.seed(1)
+	points = [(random.uniform(0,1), random.uniform(0,1)) for i in range(n)]
+
+	# Dictionary of Euclidean distance between each pair of points
+	dist = {(i, j):
+			math.sqrt(sum((points[i][k]-points[j][k])**2 for k in range(2)))
+			for i in range(n) for j in range(i)}
+			
+	return dist
+
+def solve_2tsp(dist):
+	m = gp.Model("2-TSP")
+
+	# Variables
+	vars_x = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY)
+	vars_y = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY)
+
+	# Add edges in both directions.
+	for (ix, jx), (iy, jy) in zip(vars_x.keys(), vars_y.keys()):
+		vars_x[jx, ix] = vars_x[ix, jx]
+		vars_y[jy, iy] = vars_y[iy, jy]
+
+	# Objective function
+	m.setObjective(sum(dist[e]*(vars_x[e] + vars_y[e]) for e in dist.keys()), GRB.MINIMIZE)
+
+	# Subject to:
+	# Degree of each vertex in the tour subgraph.
+	m.addConstrs(vars_x.sum(i, '*') == 2 for i in range(n))
+	m.addConstrs(vars_y.sum(i, '*') == 2 for i in range(n))
+
+	# Tours need to be edge-disjoint.
+	m.addConstrs((vars_x[e] + vars_y[e]) <= 1 for e in dist.keys())
+
+
+	# Set time limit of 30 minutes.
+	m.setParam('TimeLimit', 30*60)
+
+	# Refresh and save model.
+	m.update()
+	m.write(f'2-tsp_{n}cities.lp')
+
+	# Optimize model with lazy constraint (subtour)
+	m._vars = [vars_x,vars_y]
+	m.Params.lazyConstraints = 1
+	m.optimize(subtourelim)
+
+
+	# Get solution
+	vals_x,vals_y = m.getAttr('x', vars_x), m.getAttr('x',vars_y)
+
+	selected_x = gp.tuplelist((i, j) for i, j in vals_x.keys() if vals_x[i, j] > 0.5)
+	selected_y = gp.tuplelist((i, j) for i, j in vals_y.keys() if vals_y[i, j] > 0.5)
+
+	# Find tours from selected, assert length.
+	tour_x = subtour(selected_x)
+	assert len(tour_x) == n
+
+	tour_y = subtour(selected_y)
+	assert len(tour_y) == n
+
+	return tour_x, tour_y, m.objVal
 
 # Callback - use lazy constraints to eliminate sub-tours
 def subtourelim(model, where):
@@ -61,80 +128,21 @@ def subtour(edges):
     return cycle
 
 
-# Parse argument
-if len(sys.argv) < 2:
-    print('Usage: atividade2.py npoints')
-    sys.exit(1)
-n = int(sys.argv[1])
-
-# Create n random points
-# define the position of each point in the plane, in float coordinates [0,1]
-# the vertices' "names" are [0,n-1].
-random.seed(1)
-points = [(random.uniform(0,1), random.uniform(0,1)) for i in range(n)]
-
-
-# Dictionary of Euclidean distance between each pair of points
-dist = {(i, j):
-        math.sqrt(sum((points[i][k]-points[j][k])**2 for k in range(2)))
-        for i in range(n) for j in range(i)}
-
-m = gp.Model("2-TSP")
-
-
-### START OF MODEL ###
-
-# Variables
-vars_x = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY)
-vars_y = m.addVars(dist.keys(), obj=dist, vtype=GRB.BINARY)
-
-# Add edges in both directions.
-for (ix, jx), (iy, jy) in zip(vars_x.keys(), vars_y.keys()):
-    vars_x[jx, ix] = vars_x[ix, jx]
-    vars_y[jy, iy] = vars_y[iy, jy]
-
-# Objective function
-m.setObjective(sum(dist[e]*(vars_x[e] + vars_y[e]) for e in dist.keys()), GRB.MINIMIZE)
-
-# Subject to:
-# Degree of each vertex in the tour subgraph.
-m.addConstrs(vars_x.sum(i, '*') == 2 for i in range(n))
-m.addConstrs(vars_y.sum(i, '*') == 2 for i in range(n))
-
-# Tours need to be edge-disjoint.
-m.addConstrs((vars_x[e] + vars_y[e]) <= 1 for e in dist.keys())
-
-### END OF MODEL ###
-
-
-# Set time limit of 30 minutes.
-m.setParam('TimeLimit', 30*60)
-
-# Refresh and save model.
-m.update()
-m.write(f'2-tsp_{n}cities.lp')
-
-# Optimize model with lazy constraint (subtour)
-m._vars = [vars_x,vars_y]
-m.Params.lazyConstraints = 1
-m.optimize(subtourelim)
-
-
-# Get solution
-vals_x,vals_y = m.getAttr('x', vars_x), m.getAttr('x',vars_y)
-
-selected_x = gp.tuplelist((i, j) for i, j in vals_x.keys() if vals_x[i, j] > 0.5)
-selected_y = gp.tuplelist((i, j) for i, j in vals_y.keys() if vals_y[i, j] > 0.5)
-
-# Find tours from selected, assert length.
-tour_x = subtour(selected_x)
-assert len(tour_x) == n
-
-tour_y = subtour(selected_y)
-assert len(tour_y) == n
-
-print('')
-print('First tour: %s' % str(tour_x))
-print('Second tour: %s' % str(tour_y))
-print('Optimal cost: %g' % m.objVal)
-print('')
+if __name__ == '__main__':
+	
+	# Parse argument
+	if len(sys.argv) < 2:
+		print('Usage: atividade2.py npoints')
+		sys.exit(1)
+	n = int(sys.argv[1])
+	
+	# Create instance and solve
+	dist = generate_instance(n)
+	x,y,cost = solve_2tsp(dist)
+	
+	print('')
+	print('First tour: %s' % str(x))
+	print('Second tour: %s' % str(y))
+	print('Optimal cost: %g' % cost)
+	print('')
+	
